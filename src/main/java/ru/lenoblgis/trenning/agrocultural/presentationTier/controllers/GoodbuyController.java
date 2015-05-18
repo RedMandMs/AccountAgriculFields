f7f480324d79b37e54ddf72e3748633f275c6945
Sergey <vilgodskiy@gmail.com>
2015-05-18 13:48:04 +0300

    Работа с двумя контроллерами

---
+++ src/main/java/ru/lenoblgis/trenning/agrocultural/presentationTier/controllers/GoodbuyController.java
@@ -0,0 +1,17 @@
+package ru.lenoblgis.trenning.agrocultural.presentationTier.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GoodbuyController {
+
+	
+	@RequestMapping(value = "/goodbuy", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Goodbuy, my friend");
+        return "goodbuy";
+	}
+}
